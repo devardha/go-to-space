1f15e1e33fb6a3a980d66d7289b8c18fe959043b
devardha <ardhaydhtm@gmail.com>
2020-12-12 17:45:57 +0700

    Connect to postgres on heroku

---
+++ README.md
@@ -0,0 +1,8 @@
+# Go to Space
+Simple Space API built with Golang and Gin Web Framework
+
+## API Endpoints
+- `/api/galaxies` - GET all galaxy
+- `/api/galaxies/:id`- GET galaxy by id
+- `/api/stars` - GET all star
+- `/api/stars/:id` - GET star by id
--- main.go
+++ main.go
@@ -6,13 +6,52 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB //database
+
+// GetEnv - get variables from .env
+func GetEnv(name string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Cannot load .env file")
+	}
+
+	return os.Getenv(name)
+}
+
+// ConnectToDb - initializes the ORM and Connection to the postgres DB
+func ConnectToDb() {
+	dsn := GetEnv("CONNECTION_STRING")
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if conn != nil {
+		fmt.Println("Database connected")
+	}
+
+	db = conn
+	db.Debug().AutoMigrate() //Database migration
+}
+
+// GetDB - get database object
+func GetDB() *gorm.DB {
+	return db
+}
+
 func main() {
+	ConnectToDb()
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	port := os.Getenv("PORT")
+	//port := os.Getenv("PORT")
+	port := "5000"
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
